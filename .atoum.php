0133c692a7a52267e8b89fe0fdc672bbbcb8d89f
Tiross <Tiross@users.noreply.github.com>
2015-03-12 13:46:00 +0100

    More conf for Travis/Coveralls...

--- .atoum.php
+++ .atoum.php
@@ -1,14 +1,32 @@
 <?php
 
 use mageekguy\atoum;
+use mageekguy\atoum\reports;
+
 
 $report = $script->addDefaultReport();
-
-// This will add the atoum logo before each run.
-$report->addField(new atoum\report\fields\runner\atoum\logo());
-
-// This will add a green or red logo after each run depending on its status.
 $report->addField(new atoum\report\fields\runner\result\logo());
 
+$dir = './build/logs';
+if (!is_dir($dir)) {
+    mkdir($dir, 0777, true);
+}
+
+$cloverWriter = new atoum\writers\file($dir . '/clover.xml');
+$cloverReport = new atoum\reports\asynchronous\clover();
+$cloverReport->addWriter($cloverWriter);
+
+$runner->addReport($cloverReport);
+
 $testGenerator = new atoum\test\generator();
-$testGenerator->setTestClassesDirectory(__DIR__ . '/tests');
+$script->getRunner()->addTestsFromDirectory(__DIR__ . DIRECTORY_SEPARATOR . 'tests');
+
+
+$dir = './coverage';
+if (!is_dir($dir)) {
+    mkdir($dir, 0777, true);
+}
+
+$coverage = new atoum\report\fields\runner\coverage\html('json', $dir);
+$coverage->setRootUrl('http://localhost');
+$report->addField($coverage);
